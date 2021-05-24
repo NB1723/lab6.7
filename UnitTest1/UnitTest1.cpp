18af655ab5b225a17609bb3c2ca107b5d7921e17
NB1723 <notalia03@gmail.com>
2021-05-24 07:58:15 +0300

    lab6.7

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Array a = { new int[5]{ 10, 7, 5, 4, 1 }, 5 };
+			IFilter* f = new FilterEven;
+			Array c = ArrayFilter(a, *f);
+			Assert::AreEqual(*(c.start + 1), 4);
+		}
+	};
+}
